8d7e7d29ad783cd63feabafa6db961dccfaa4c39
Nambinina <nambinina.solofonaina@gmail.com>
2019-08-13 21:49:17 +0300

    Ajout session

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
   resources :user
   resources :gossips do
     resources :comments
   end
 	get 'user/:id', to:"user#show"
   get 'gossip/:id', to: 'gossip#show'
+
+  root "welcome#show"
+  resources :welcome, only: [:show]
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
